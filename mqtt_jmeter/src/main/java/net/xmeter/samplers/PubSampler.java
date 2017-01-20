ae6da770ae73e14963e7462d1acca4f90c349677
RockyJin <fahua.jin@gmail.com>
2017-01-20 10:26:21 +0800

    format the code

--- mqtt_jmeter/src/main/java/net/xmeter/samplers/PubSampler.java
+++ mqtt_jmeter/src/main/java/net/xmeter/samplers/PubSampler.java
@@ -27,7 +27,7 @@ public class PubSampler extends AbstractMQTTSampler implements ThreadListener {
 	private String payload = null;
 	private String clientId = "";
 	private QoS qos_enum = QoS.AT_MOST_ONCE;
-	
+
 	public int getQOS() {
 		return getPropertyAsInt(QOS_LEVEL, QOS_0);
 	}
@@ -35,7 +35,7 @@ public class PubSampler extends AbstractMQTTSampler implements ThreadListener {
 	public void setQOS(int qos) {
 		setProperty(QOS_LEVEL, qos);
 	}
-	
+
 	public String getTopic() {
 		return getPropertyAsString(TOPIC_NAME, DEFAULT_TOPIC_NAME);
 	}
@@ -43,27 +43,27 @@ public class PubSampler extends AbstractMQTTSampler implements ThreadListener {
 	public void setTopic(String topicName) {
 		setProperty(TOPIC_NAME, topicName);
 	}
-	
+
 	public boolean isAddTimestamp() {
 		return getPropertyAsBoolean(ADD_TIMESTAMP);
 	}
-	
+
 	public void setAddTimestamp(boolean addTimestamp) {
 		setProperty(ADD_TIMESTAMP, addTimestamp);
 	}
-	
+
 	public String getMessageType() {
 		return getPropertyAsString(MESSAGE_TYPE, MESSAGE_TYPE_RANDOM_STR_WITH_FIX_LEN);
 	}
-	
+
 	public void setMessageType(String messageType) {
 		setProperty(MESSAGE_TYPE, messageType);
 	}
-	
+
 	public int getMessageLength() {
 		return getPropertyAsInt(MESSAGE_FIX_LENGTH, DEFAULT_MESSAGE_FIX_LENGTH);
 	}
-	
+
 	public void setMessageLength(int length) {
 		setProperty(MESSAGE_FIX_LENGTH, length);
 	}
@@ -71,11 +71,11 @@ public class PubSampler extends AbstractMQTTSampler implements ThreadListener {
 	public String getMessage() {
 		return getPropertyAsString(MESSAGE_TO_BE_SENT, "");
 	}
-	
+
 	public void setMessage(String message) {
 		setProperty(MESSAGE_TO_BE_SENT, message);
 	}
-	
+
 	public String getConnPrefix() {
 		return getPropertyAsString(CONN_CLIENT_ID_PREFIX, DEFAULT_CONN_PREFIX_FOR_PUB);
 	}
@@ -86,33 +86,33 @@ public class PubSampler extends AbstractMQTTSampler implements ThreadListener {
 		try {
 			String topicName = getTopic();
 			String actualPayload = payload;
-			
-			if(MESSAGE_TYPE_HEX_STRING.equals(getMessageType())) {
+
+			if (MESSAGE_TYPE_HEX_STRING.equals(getMessageType())) {
 				actualPayload = getMessage();
-				//TODO Change it to the binary hex.
-			} else if(MESSAGE_TYPE_STRING.equals(getMessageType())) {
+				// TODO Change it to the binary hex.
+			} else if (MESSAGE_TYPE_STRING.equals(getMessageType())) {
 				actualPayload = getMessage();
 			}
-			if(isAddTimestamp()) {
+			if (isAddTimestamp()) {
 				actualPayload = (System.currentTimeMillis() + TIME_STAMP_SEP_FLAG) + payload;
 			}
-			result.sampleStart(); 
-			
+			result.sampleStart();
+
 			Future<Void> pub = connection.publish(topicName, actualPayload.getBytes(), qos_enum, false);
 			pub.await();
-			
-			result.sampleEnd(); 
-            result.setSuccessful(true);
-            result.setResponseData((MessageFormat.format("Publish Successful by {0}.", clientId)).getBytes());
-            result.setResponseMessage(MessageFormat.format("publish successfully via Connection {0}.", connection));
-            result.setResponseCodeOK(); 
-		} catch(Exception ex) {
+
+			result.sampleEnd();
+			result.setSuccessful(true);
+			result.setResponseData((MessageFormat.format("Publish Successful by {0}.", clientId)).getBytes());
+			result.setResponseMessage(MessageFormat.format("publish successfully via Connection {0}.", connection));
+			result.setResponseCodeOK();
+		} catch (Exception ex) {
 			logger.log(Priority.ERROR, ex.getMessage(), ex);
-			result.sampleEnd(); 
-            result.setSuccessful(false);
-            result.setResponseMessage(MessageFormat.format("Connection {0} connected failed.", connection));
-            result.setResponseData("Failed.".getBytes());
-            result.setResponseCode("500"); 
+			result.sampleEnd();
+			result.setSuccessful(false);
+			result.setResponseMessage(MessageFormat.format("Connection {0} connected failed.", connection));
+			result.setResponseData("Failed.".getBytes());
+			result.setResponseCode("500");
 		}
 		return result;
 	}
@@ -122,23 +122,23 @@ public class PubSampler extends AbstractMQTTSampler implements ThreadListener {
 		try {
 			mqtt.setHost(getProtocol().toLowerCase() + "://" + getServer() + ":" + getPort());
 			mqtt.setKeepAlive((short) getConnKeepAlive());
-			
+
 			clientId = Util.generateClientId(getConnPrefix());
 			mqtt.setClientId(clientId);
-			
+
 			mqtt.setConnectAttemptsMax(getConnAttamptMax());
 			mqtt.setReconnectAttemptsMax(getConnReconnAttamptMax());
-			
-			if(!"".equals(getUserNameAuth().trim())) {
+
+			if (!"".equals(getUserNameAuth().trim())) {
 				mqtt.setUserName(getUserNameAuth());
 			}
-			if(!"".equals(getPasswordAuth().trim())) {
+			if (!"".equals(getPasswordAuth().trim())) {
 				mqtt.setPassword(getPasswordAuth());
 			}
-			if(MESSAGE_TYPE_RANDOM_STR_WITH_FIX_LEN.equals(getMessageType())) {
-				payload = Util.generatePayload(getMessageLength());	
+			if (MESSAGE_TYPE_RANDOM_STR_WITH_FIX_LEN.equals(getMessageType())) {
+				payload = Util.generatePayload(getMessageLength());
 			}
-			
+
 			switch (getQOS()) {
 			case 0:
 				qos_enum = QoS.AT_MOST_ONCE;
@@ -152,7 +152,7 @@ public class PubSampler extends AbstractMQTTSampler implements ThreadListener {
 			default:
 				break;
 			}
-			
+
 			connection = mqtt.futureConnection();
 			Future<Void> f1 = connection.connect();
 			f1.await(getConnTimeout(), TimeUnit.SECONDS);
@@ -160,12 +160,12 @@ public class PubSampler extends AbstractMQTTSampler implements ThreadListener {
 			logger.log(Priority.ERROR, e.getMessage(), e);
 		}
 	}
-	
+
 	@Override
 	public void threadFinished() {
-		if(this.connection != null) {
+		if (this.connection != null) {
 			this.connection.disconnect();
-			logger.info(MessageFormat.format("The connection {0} disconneted successfully.", connection));	
-		} 
+			logger.info(MessageFormat.format("The connection {0} disconneted successfully.", connection));
+		}
 	}
 }
--- mqtt_jmeter/src/main/java/net/xmeter/samplers/SubSampler.java
+++ mqtt_jmeter/src/main/java/net/xmeter/samplers/SubSampler.java
@@ -28,20 +28,20 @@ public class SubSampler extends AbstractMQTTSampler implements ThreadListener {
 	private transient MQTT mqtt = new MQTT();
 	private transient CallbackConnection connection = null;
 	private transient static Logger logger = LoggingManager.getLoggerForClass();
-	
+
 	private boolean connectFailed = false;
 	private boolean subFailed = false;
 	private boolean receivedMsgFailed = false;
-	
+
 	private int receivedMessageSize = 0;
 	private int receivedCount = 0;
 	private double avgElapsedTime = 0f;
 
 	private List<String> contents = new ArrayList<String>();
 	private boolean printFlag = false;
-	
+
 	private Object lock = new Object();
-	
+
 	private int qos = QOS_0;
 	/**
 	 * 
@@ -55,7 +55,7 @@ public class SubSampler extends AbstractMQTTSampler implements ThreadListener {
 	public void setQOS(int qos) {
 		setProperty(QOS_LEVEL, qos);
 	}
-	
+
 	public String getTopic() {
 		return getPropertyAsString(TOPIC_NAME, DEFAULT_TOPIC_NAME);
 	}
@@ -63,23 +63,23 @@ public class SubSampler extends AbstractMQTTSampler implements ThreadListener {
 	public void setTopic(String topicName) {
 		setProperty(TOPIC_NAME, topicName);
 	}
-	
+
 	public boolean isAddTimestamp() {
 		return getPropertyAsBoolean(ADD_TIMESTAMP);
 	}
-	
+
 	public void setAddTimestamp(boolean addTimestamp) {
 		setProperty(ADD_TIMESTAMP, addTimestamp);
 	}
-	
+
 	public boolean isDebugResponse() {
 		return getPropertyAsBoolean(DEBUG_RESPONSE, false);
 	}
-	
+
 	public void setDebugResponse(boolean debugResponse) {
 		setProperty(DEBUG_RESPONSE, debugResponse);
 	}
-	
+
 	public String getConnPrefix() {
 		return getPropertyAsString(CONN_CLIENT_ID_PREFIX, DEFAULT_CONN_PREFIX_FOR_SUB);
 	}
@@ -87,48 +87,48 @@ public class SubSampler extends AbstractMQTTSampler implements ThreadListener {
 	@Override
 	public SampleResult sample(Entry arg0) {
 		SampleResult result = new SampleResult();
-		
+
 		long startTime = System.currentTimeMillis();
 		result.sampleStart();
-		
-		if(connectFailed) {
+
+		if (connectFailed) {
 			return fillFailedResult(result, MessageFormat.format("Connection {0} connected failed.", connection));
-		} else if(subFailed) {
+		} else if (subFailed) {
 			return fillFailedResult(result, "Failed to subscribe to topic.");
-		} else if(receivedMsgFailed) {
+		} else if (receivedMsgFailed) {
 			return fillFailedResult(result, "Failed to receive message.");
 		}
-		
+
 		synchronized (lock) {
 			String message = MessageFormat.format("Received {0} of message\n.", receivedCount);
-			//getLogger().info(message);
+			// getLogger().info(message);
 			StringBuffer content = new StringBuffer("");
-			if(isDebugResponse()) {
-				for(int i = 0; i < contents.size(); i++) {
+			if (isDebugResponse()) {
+				for (int i = 0; i < contents.size(); i++) {
 					content.append(contents.get(i) + " \n");
-				}	
+				}
 			}
-			//System.out.println(MessageFormat.format("receivedMessageSize {0} with receivedCount {1}.", receivedMessageSize, receivedCount));
+			// System.out.println(MessageFormat.format("receivedMessageSize {0}
+			// with receivedCount {1}.", receivedMessageSize, receivedCount));
 			int avgSize = 0;
-			if(receivedCount != 0) {
+			if (receivedCount != 0) {
 				avgSize = receivedMessageSize / receivedCount;
 			}
 			result = fillOKResult(result, avgSize, message, content.toString());
-			if(isAddTimestamp()) {
-				result.setEndTime(startTime + (long)this.avgElapsedTime);	
+			if (isAddTimestamp()) {
+				result.setEndTime(startTime + (long) this.avgElapsedTime);
 			}
 			result.setSampleCount(receivedCount);
-			
+
 			receivedMessageSize = 0;
 			receivedCount = 0;
 			avgElapsedTime = 0f;
 			contents.clear();
-			
+
 			return result;
 		}
 	}
-	
-	
+
 	private SampleResult fillFailedResult(SampleResult result, String message) {
 		result.setResponseCode("500");
 		result.setSuccessful(false);
@@ -137,7 +137,7 @@ public class SubSampler extends AbstractMQTTSampler implements ThreadListener {
 		result.sampleEnd();
 		return result;
 	}
-	
+
 	private SampleResult fillOKResult(SampleResult result, int size, String message, String contents) {
 		result.setResponseCode("200");
 		result.setSuccessful(true);
@@ -156,6 +156,7 @@ public class SubSampler extends AbstractMQTTSampler implements ThreadListener {
 			public void onSuccess(Void value) {
 				logger.info(MessageFormat.format("Connection {0} disconnect successfully.", connection));
 			}
+
 			@Override
 			public void onFailure(Throwable value) {
 				logger.info(MessageFormat.format("Connection {0} failed to disconnect.", connection));
@@ -168,20 +169,20 @@ public class SubSampler extends AbstractMQTTSampler implements ThreadListener {
 		try {
 			mqtt.setHost(getProtocol().toLowerCase() + "://" + getServer() + ":" + getPort());
 			mqtt.setKeepAlive((short) getConnKeepAlive());
-			
+
 			String clientId = Util.generateClientId(getConnPrefix());
 			mqtt.setClientId(clientId);
-			
+
 			mqtt.setConnectAttemptsMax(getConnAttamptMax());
 			mqtt.setReconnectAttemptsMax(getConnReconnAttamptMax());
-			
-			if(!"".equals(getUserNameAuth().trim())) {
+
+			if (!"".equals(getUserNameAuth().trim())) {
 				mqtt.setUserName(getUserNameAuth());
 			}
-			if(!"".equals(getPasswordAuth().trim())) {
+			if (!"".equals(getPasswordAuth().trim())) {
 				mqtt.setPassword(getPasswordAuth());
 			}
-			
+
 			connection = mqtt.callbackConnection();
 			connection.listener(new Listener() {
 				@Override
@@ -192,19 +193,19 @@ public class SubSampler extends AbstractMQTTSampler implements ThreadListener {
 						String msg = baos.toString();
 						ack.run();
 						synchronized (lock) {
-							if(isAddTimestamp()) {
+							if (isAddTimestamp()) {
 								long now = System.currentTimeMillis();
 								int index = msg.indexOf(TIME_STAMP_SEP_FLAG);
-								if(index == -1 && (!printFlag)) {
+								if (index == -1 && (!printFlag)) {
 									logger.info("Payload does not include timestamp: " + msg);
 									printFlag = true;
-								} else if(index != -1) {
+								} else if (index != -1) {
 									long start = Long.parseLong(msg.substring(0, index));
 									long elapsed = now - start;
 									avgElapsedTime = (avgElapsedTime * receivedCount + elapsed) / (receivedCount + 1);
 								}
 							}
-							if(isDebugResponse()) {
+							if (isDebugResponse()) {
 								contents.add(msg);
 							}
 							receivedMessageSize += msg.length();
@@ -214,37 +215,41 @@ public class SubSampler extends AbstractMQTTSampler implements ThreadListener {
 						logger.log(Priority.ERROR, e.getMessage(), e);
 					}
 				}
+
 				@Override
 				public void onFailure(Throwable value) {
 					connectFailed = true;
 					connection.kill(null);
 				}
+
 				@Override
 				public void onDisconnected() {
 				}
+
 				@Override
 				public void onConnected() {
 				}
 			});
-			
+
 			connection.connect(new Callback<Void>() {
 				@Override
 				public void onSuccess(Void value) {
 					String topicName = getTopic();
 					Topic[] topics = new Topic[1];
-					if(qos == QOS_0) {
-						topics[0] = new Topic(topicName, QoS.AT_MOST_ONCE);	
-					} else if(qos == QOS_1) {
+					if (qos == QOS_0) {
+						topics[0] = new Topic(topicName, QoS.AT_MOST_ONCE);
+					} else if (qos == QOS_1) {
 						topics[0] = new Topic(topicName, QoS.AT_LEAST_ONCE);
 					} else {
 						topics[0] = new Topic(topicName, QoS.EXACTLY_ONCE);
 					}
-					
+
 					connection.subscribe(topics, new Callback<byte[]>() {
 						@Override
 						public void onSuccess(byte[] value) {
 							logger.info("sub successful: " + new String(value));
 						}
+
 						@Override
 						public void onFailure(Throwable value) {
 							subFailed = true;
@@ -252,6 +257,7 @@ public class SubSampler extends AbstractMQTTSampler implements ThreadListener {
 						}
 					});
 				}
+
 				@Override
 				public void onFailure(Throwable value) {
 					connectFailed = true;
@@ -260,7 +266,7 @@ public class SubSampler extends AbstractMQTTSampler implements ThreadListener {
 		} catch (Exception e) {
 			logger.log(Priority.ERROR, e.getMessage(), e);
 		}
-	
+
 	}
-	
+
 }
