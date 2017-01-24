f79afc1ed4d41c10e700fed3b00410727a07064e
RockyJin <fahua.jin@gmail.com>
2017-01-24 13:32:24 +0800

    format conn sampler code

--- mqtt_jmeter/src/main/java/net/xmeter/samplers/ConnectionSampler.java
+++ mqtt_jmeter/src/main/java/net/xmeter/samplers/ConnectionSampler.java
@@ -22,7 +22,8 @@ import org.fusesource.mqtt.client.Topic;
 
 import net.xmeter.Util;
 
-public class ConnectionSampler extends AbstractMQTTSampler implements TestStateListener, ThreadListener, Interruptible, SampleListener {
+public class ConnectionSampler extends AbstractMQTTSampler
+		implements TestStateListener, ThreadListener, Interruptible, SampleListener {
 	private transient static Logger logger = LoggingManager.getLoggerForClass();
 	private transient MQTT mqtt = new MQTT();
 	private transient FutureConnection connection = null;
@@ -36,48 +37,48 @@ public class ConnectionSampler extends AbstractMQTTSampler implements TestStateL
 	public SampleResult sample(Entry entry) {
 		SampleResult result = new SampleResult();
 		try {
-			if(!DEFAULT_PROTOCOL.equals(getProtocol())) {
+			if (!DEFAULT_PROTOCOL.equals(getProtocol())) {
 				mqtt.setSslContext(Util.getContext(this));
 			}
-			
+
 			mqtt.setHost(getProtocol().toLowerCase() + "://" + getServer() + ":" + getPort());
 			mqtt.setKeepAlive((short) getConnKeepAlive());
 			String clientId = Util.generateClientId(getConnPrefix());
 			mqtt.setClientId(clientId);
 			mqtt.setConnectAttemptsMax(getConnAttamptMax());
 			mqtt.setReconnectAttemptsMax(getConnReconnAttamptMax());
-			
-			if(!"".equals(getUserNameAuth().trim())) {
+
+			if (!"".equals(getUserNameAuth().trim())) {
 				mqtt.setUserName(getUserNameAuth());
 			}
-			if(!"".equals(getPasswordAuth().trim())) {
+			if (!"".equals(getPasswordAuth().trim())) {
 				mqtt.setPassword(getPasswordAuth());
 			}
-			
-			result.sampleStart(); 
+
+			result.sampleStart();
 			connection = mqtt.futureConnection();
 			Future<Void> f1 = connection.connect();
 			f1.await(getConnTimeout(), TimeUnit.SECONDS);
-			
-			Topic[] topics = {new Topic("topic_"+ clientId, QoS.AT_LEAST_ONCE)};
+
+			Topic[] topics = { new Topic("topic_" + clientId, QoS.AT_LEAST_ONCE) };
 			connection.subscribe(topics);
-			
-			result.sampleEnd(); 
-            result.setSuccessful(true);
-            result.setResponseData("Successful.".getBytes());
-            result.setResponseMessage(MessageFormat.format("Connection {0} connected successfully.", connection));
-            result.setResponseCodeOK(); 
-		} catch(Exception e) {
+
+			result.sampleEnd();
+			result.setSuccessful(true);
+			result.setResponseData("Successful.".getBytes());
+			result.setResponseMessage(MessageFormat.format("Connection {0} connected successfully.", connection));
+			result.setResponseCodeOK();
+		} catch (Exception e) {
 			logger.log(Priority.ERROR, e.getMessage(), e);
-			result.sampleEnd(); 
-            result.setSuccessful(false);
-            result.setResponseMessage(MessageFormat.format("Connection {0} connected failed.", connection));
-            result.setResponseData("Failed.".getBytes());
-            result.setResponseCode("500"); 
+			result.sampleEnd();
+			result.setSuccessful(false);
+			result.setResponseMessage(MessageFormat.format("Connection {0} connected failed.", connection));
+			result.setResponseData("Failed.".getBytes());
+			result.setResponseCode("500");
 		}
 		return result;
 	}
-	
+
 	@Override
 	public void testEnded() {
 		this.testEnded("local");
@@ -90,7 +91,7 @@ public class ConnectionSampler extends AbstractMQTTSampler implements TestStateL
 
 	@Override
 	public void testStarted() {
-		
+
 	}
 
 	@Override
@@ -99,26 +100,27 @@ public class ConnectionSampler extends AbstractMQTTSampler implements TestStateL
 
 	@Override
 	public void threadFinished() {
-		if(JMeter.isNonGUI()) {
+		if (JMeter.isNonGUI()) {
 			logger.info("The work has been done, will sleep current thread for " + getConnKeepTime() + " sceconds.");
 			sleepCurrentThreadAndDisconnect();
 		}
 	}
-	
+
 	private void sleepCurrentThreadAndDisconnect() {
 		try {
 			long start = System.currentTimeMillis();
-			while((System.currentTimeMillis() - start) <= TimeUnit.SECONDS.toMillis(getConnKeepTime())) {
-				if(this.interrupt) {
+			while ((System.currentTimeMillis() - start) <= TimeUnit.SECONDS.toMillis(getConnKeepTime())) {
+				if (this.interrupt) {
 					logger.info("interrupted flag is true, and stop the sleep.");
 					break;
 				}
-				TimeUnit.SECONDS.sleep(1);		
+				TimeUnit.SECONDS.sleep(1);
 			}
-			
-			if(connection != null) {
+
+			if (connection != null) {
 				connection.disconnect();
-				logger.log(Priority.INFO, MessageFormat.format("The connection {0} disconneted successfully.", connection));	
+				logger.log(Priority.INFO,
+						MessageFormat.format("The connection {0} disconneted successfully.", connection));
 			}
 		} catch (InterruptedException e) {
 			logger.log(Priority.ERROR, e.getMessage(), e);
@@ -127,13 +129,13 @@ public class ConnectionSampler extends AbstractMQTTSampler implements TestStateL
 
 	@Override
 	public void threadStarted() {
-		
+
 	}
 
 	@Override
 	public boolean interrupt() {
 		this.interrupt = true;
-		if(!JMeter.isNonGUI()) {
+		if (!JMeter.isNonGUI()) {
 			logger.info("In GUI mode, received the interrupt request from user.");
 		}
 		return true;
@@ -144,8 +146,9 @@ public class ConnectionSampler extends AbstractMQTTSampler implements TestStateL
 	 */
 	@Override
 	public void sampleOccurred(SampleEvent event) {
-		if(!JMeter.isNonGUI()) {
-			logger.info("Created the sampler results, will sleep current thread for " + getConnKeepTime() + " sceconds");
+		if (!JMeter.isNonGUI()) {
+			logger.info(
+					"Created the sampler results, will sleep current thread for " + getConnKeepTime() + " sceconds");
 			sleepCurrentThreadAndDisconnect();
 		}
 	}
